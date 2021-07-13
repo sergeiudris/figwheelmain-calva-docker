92962d79700490f55f723d5921ee73909d1c8d91
Sergei Udris <sergei.udris@gmail.com>
2021-07-13 14:25:32 +0300

    Revert "remove license  - repo is code only"
    This reverts commit 609d1f7a3100274a535774d6fa296ededebc76e4.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject hello-world.core "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
